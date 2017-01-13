20e5b678faca30aaa0975327f2d8f3deaf727d97
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2017-01-13 09:46:18 -0500

    Android Studio scaffolding

---
+++ Android/Arti-Chat/app/src/main/java/team_rick/arti_chat/LoginActivity.java
@@ -0,0 +1,13 @@
+package team_rick.arti_chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
