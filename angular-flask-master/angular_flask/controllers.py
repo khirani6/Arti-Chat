40e1e93de9b5d7bce41fd94fa6b450fb368c05e7
bmawji3 <bmawji3@gatech.edu>
2017-01-25 16:19:04 -0500

    need to fix authorize_url

--- angular-flask-master/angular_flask/controllers.py
+++ angular-flask-master/angular_flask/controllers.py
@@ -1,20 +1,20 @@
 import os
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, session
 from flask import render_template, url_for, redirect, send_from_directory
 from flask import send_file, make_response, abort
 
 from angular_flask import app
 
 # routing for API endpoints, generated from the models designated as API_MODELS
-from angular_flask.core import api_manager
+from angular_flask.core import api_manager, etsy
 from angular_flask.models import *
 
 for model_name in app.config['API_MODELS']:
     model_class = app.config['API_MODELS'][model_name]
     api_manager.create_api(model_class, methods=['GET', 'POST'])
 
-session = api_manager.session
+api_session = api_manager.session
 
 
 # routing for basic pages (pass routing onto the Angular app)
@@ -24,6 +24,35 @@ session = api_manager.session
 @app.route('/login')
 def basic_pages(**kwargs):
     return make_response(open('angular_flask/templates/index.html').read())
+
+
+@etsy.tokengetter
+def get_etsy_token(token=None):
+    return session.get('etsy_token')
+
+
+@app.route('/login2')
+def login2():
+    return etsy.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
+
+
+@app.route('/oauth-authorized')
+def oauth_authorized():
+    next_url = request.args.get('next') or url_for('basic_pages')
+    resp = etsy.authorized_response()
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    session['etsy_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['etsy_user'] = resp['screen_name']
+
+    flash('You were signed in as %s' % resp['screen_name'])
+    return redirect(next_url)
 
 
 # routing for CRUD-style endpoints
@@ -38,7 +67,7 @@ crud_url_models = app.config['CRUD_URL_MODELS']
 def rest_pages(model_name, item_id=None):
     if model_name in crud_url_models:
         model_class = crud_url_models[model_name]
-        if item_id is None or session.query(exists().where(
+        if item_id is None or api_session.query(exists().where(
                 model_class.id == item_id)).scalar():
             return make_response(open(
                 'angular_flask/templates/index.html').read())
