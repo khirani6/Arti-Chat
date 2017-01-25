ee58bf111f8667a715b6449cf5e8724827e4afe5
bmawji3 <bmawji3@gatech.edu>
2017-01-25 16:19:04 -0500

    need to fix authorize_url

--- angular-flask-master/angular_flask/core.py
+++ angular-flask-master/angular_flask/core.py
@@ -2,7 +2,19 @@ from angular_flask import app
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
+from flask_oauthlib.client import OAuth
 
 db = SQLAlchemy(app)
+oauth = OAuth()
+
+etsy = oauth.remote_app('etsy',
+    base_url='https://openapi.etsy.com/v2',
+    request_token_url='https://openapi.etsy.com/v2/oauth/request_token',
+    access_token_url='https://openapi.etsy.com/v2/oauth/access_token',
+    # Need to fix authorize_url
+    authorize_url='https://openapi.etsy.com/v2/oauth/authenticate',
+    consumer_key='jq4x92ncxfk5tawous7dcaxm',
+    consumer_secret='5ywk81qg7d'
+)
 
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
