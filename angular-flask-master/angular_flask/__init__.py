f426c63a94dd32b008225652a4c66967f9102501
Robert Bradshaw <rbradshaw6@gatech.edu>
2017-01-28 22:04:01 -0500

    cors attempt

--- angular-flask-master/angular_flask/__init__.py
+++ angular-flask-master/angular_flask/__init__.py
@@ -2,8 +2,11 @@ import os
 import json
 from flask import Flask, request, Response
 from flask import render_template, send_from_directory, url_for
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.config.from_object('angular_flask.settings')
 
