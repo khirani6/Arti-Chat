cea3630955f6df7c3b046b4c5f9b907b6b4ee476
rbradshaw6 <rbradsahw6@gatech.edu>
2017-02-18 15:14:40 -0500

    Updated from Angular 1.0.x to Angular 1.6.1 in preparation for addition of lightbox modal plugin.

--- angular-flask-master/angular_flask/static/js/app.js
+++ angular-flask-master/angular_flask/static/js/app.js
@@ -1,40 +1,44 @@
-'use strict';
+"use strict";
 
-angular.module('AngularFlask', ['angularFlaskServices'])
-	.config(['$routeProvider', '$locationProvider',
-		function($routeProvider, $locationProvider) {
-    		$routeProvider.when('/', {
-    			templateUrl: 'static/partials/login.html',
-    			controller: LoginController
-    		})
-    		.when('/about', {
-    			templateUrl: 'static/partials/about.html',
-    			controller: AboutController
-    		})
-    		.when('/post', {
-    			templateUrl: 'static/partials/post-list.html',
-    			controller: PostListController
-    		})
-    		.when('/post/:postId', {
-    			templateUrl: '/static/partials/post-detail.html',
-    			controller: PostDetailController
-    		})
-    		/* Create a "/blog" route that takes the user to the same place as "/post" */
-    		.when('/blog', {
-    			templateUrl: 'static/partials/post-list.html',
-    			controller: PostListController
-    		})
-    		.when('/login', {
-    			templateUrl: 'static/partials/login.html',
-    			controller: LoginController
-    		})
-    		.when('/home', {
-    			templateUrl: 'static/partials/home2.html',
-    			controller: HomeController
-    		})
-    		.otherwise({
-    			redirectTo: '/'
-    		});
+angular.module("AngularFlask", ["ui.bootstrap", "ngRoute", "ngResource", "angularFlaskServices", "bootstrapLightbox"]).config(["$routeProvider", "$locationProvider", "LightboxProvider",
+	function($routeProvider, $locationProvider, LightboxProvider) {
+		$routeProvider.when("/", {
+			templateUrl: "static/partials/login.html",
+			controller: LoginController
+		})
+		.when("/about", {
+			templateUrl: "static/partials/about.html",
+			controller: AboutController
+		})
+		.when("/post", {
+			templateUrl: "static/partials/post-list.html",
+			controller: PostListController
+		})
+		.when("/post/:postId", {
+			templateUrl: "/static/partials/post-detail.html",
+			controller: PostDetailController
+		})
+		/* Create a "/blog" route that takes the user to the same place as "/post" */
+		.when("/blog", {
+			templateUrl: "static/partials/post-list.html",
+			controller: PostListController
+		})
+		.when("/login", {
+			templateUrl: "static/partials/login.html",
+			controller: LoginController
+		})
+		.when("/home", {
+			templateUrl: "static/partials/home2.html",
+			controller: HomeController
+		})
+		.otherwise({
+			redirectTo: "/"
+		});
 
-    		$locationProvider.html5Mode(true);
-	}]);
+		$locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        LightboxProvider.templateUrl = "../templates/lightbox.html";
+}]);
