c3e72d267ed0fe8909f2b9c5ac683de8b7fe621b
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2017-04-19 08:24:47 -0400

    post to both fb and twitter

--- angular-flask-master/angular_flask/static/js/controllers.js
+++ angular-flask-master/angular_flask/static/js/controllers.js
@@ -302,11 +302,13 @@ function HomeController($scope, $window, $q, $http, Shops, $rootScope, ActiveSho
 
     $scope.postAnnouncement = function() {
         if (fb) {
-            facebookService.postMessage($scope.announcementText);
-        }
-        if (twitter) {
-            var url = "/tweet/" + $scope.announcementText;
-            $window.location.href=url;
+            facebookService.postMessage($scope.announcementText)
+                .then(function (response) {
+                    if (twitter) {
+                        var url = "/tweet/" + $scope.announcementText;
+                        $window.location.href=url;
+                    }
+                });
         }
 
     }
